e013cd473c45ff2d2ab4fbc0dfc660a40ee59d96
CarrloX <42298361+CarrloX@users.noreply.github.com>
2024-05-27 07:27:14 -0500

    Proyecto Inicializado

---
+++ src/main/java/com/riwi/demo/SimulacroSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacroSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacroSpringBootApplication.class, args);
+	}
+
+}
