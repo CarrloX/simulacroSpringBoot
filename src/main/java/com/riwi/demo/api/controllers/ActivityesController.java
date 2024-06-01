8889a6e09f8788926c4f7217bd9bae74ee4f2428
Carlox <carlito1999@live.com>
2024-06-01 15:15:04 -0500

    crud alfa de actividades

--- src/main/java/com/riwi/demo/api/controllers/ActivityesController.java
+++ src/main/java/com/riwi/demo/api/controllers/ActivityesController.java
@@ -1,15 +1,23 @@
 package com.riwi.demo.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.riwi.demo.api.dto.request.ActivityesReq;
+import com.riwi.demo.api.dto.request.CoursesReq;
 import com.riwi.demo.api.dto.response.ActivityesResp;
+import com.riwi.demo.api.dto.response.CoursesResp;
 import com.riwi.demo.infrastructure.abstract_services.IActivityService;
 
 import lombok.AllArgsConstructor;
@@ -22,9 +30,36 @@ public class ActivityesController {
     @Autowired
     private IActivityService activityService;
 
+    @GetMapping
+    public ResponseEntity<Page<ActivityesResp>> getAll(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        return ResponseEntity.ok(this.activityService.getAll(page - 1, size));
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ActivityesResp> get(
+            @PathVariable String id) {
+        return ResponseEntity.ok(this.activityService.get(id));
+    }
+
+    @PutMapping(path = "/{id}")
+    public ResponseEntity<ActivityesResp> update(
+            @Validated @RequestBody ActivityesReq request,
+            @PathVariable String id) {
+        return ResponseEntity.ok(this.activityService.update(request, id));
+    }
+
     @PostMapping
     public ResponseEntity<ActivityesResp> insert(
             @Validated @RequestBody ActivityesReq request) {
         return ResponseEntity.ok(this.activityService.create(request));
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        this.activityService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/riwi/demo/infrastructure/service/ActivityService.java
+++ src/main/java/com/riwi/demo/infrastructure/service/ActivityService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.riwi.demo.api.dto.request.ActivityesReq;
@@ -52,26 +53,39 @@ public class ActivityService implements IActivityService {
 
     @Override
     public ActivityesResp get(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'get'");
+        return this.entityToResponse(this.find(id));
     }
 
     @Override
     public ActivityesResp update(ActivityesReq request, String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        
+        Activityes activity = this.find(id);
+
+        Lessons lesson = this.lessonsRepository.findById(request.getLesson_id())
+                .orElseThrow(() -> new BadRequestException(ErrorMessage.idNotFound("lesson")));
+
+        activity=this.requestToEntity(request);
+        
+        activity.setLesson_id(lesson);
+        activity.setActivity_id(id);
+        
+        return this.entityToResponse(this.activityRepository.save(activity));
     }
 
     @Override
     public void delete(String id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+        this.activityRepository.delete(this.find(id));
     }
 
     @Override
     public Page<ActivityesResp> getAll(int Page, int size) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+        if (Page < 0)
+            Page = 0;
+
+        PageRequest pagination = PageRequest.of(Page, size);
+
+        return this.activityRepository.findAll(pagination)
+                .map(activity -> this.entityToResponse(activity));
     }
 
     private ActivityesResp entityToResponse(Activityes entity) {
